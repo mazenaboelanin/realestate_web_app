f5b56079a3b83d547e1290631ec5d5a6074f50c7
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 16:29:45 +0200

    setup router for apartments pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import RootLayout from './pages/RootLayout/RootLayout';
+import { ApartmentsListing } from './pages/apartments/ApartmentsListing/ApartmentsListing';
+import ApartmentDetails from './pages/apartments/ApatmentDetails/ApartmentDetails';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      
+      { path: '/', element: <Navigate to="/apartments" replace={true} /> },
+      { path: '/apartments', element: <ApartmentsListing /> },
+      { path: '/apartments/:id', element: <ApartmentDetails /> },
     ],
   }
 ]);
