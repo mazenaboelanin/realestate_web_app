4b06a8f2f322020a54e2996e8d3233b368d52cab
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 16:55:34 +0200

    fetch and list apartments

--- src/pages/apartments/ApartmentsListing/ApartmentsListing.tsx
+++ src/pages/apartments/ApartmentsListing/ApartmentsListing.tsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
+import Apartment from "../../../models/Apartment";
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
+
 export const ApartmentsListing = () => {
+  const [apartments, setApartments] = useState<Apartment[]>([]);
+
+  useEffect(() => {
+    async function fetchApartments() {
+      try {
+        const res = await axios.get('http://localhost:5000/api/v1/apartments');
+        console.log(res.data.response);
+        setApartments(res.data.response);
+
+      } catch (error) {
+        throw new Error('Error fetching apartments');
+      }
+    }
+    fetchApartments();
+  }, []);
+
   return (
     <div>
       <h1>ApartmentsListing</h1>
       <ul>
-        <li> Aparmtnet 1</li>
-        <li> Aparmtnet 2</li>
-        <li> Aparmtnet 3</li>
+        {apartments.map((apartment) => (
+          <li 
+            key={apartment.id} 
+          >
+            {apartment?.title}
+          </li>
+        ))}
       </ul>
     </div>
   );
