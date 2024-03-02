c1d04a7887c772f72174a280e2634ed906a5a716
mazenaboelanin <mazenaboelanin@gmail.com>
2024-03-02 17:56:09 +0200

    add container to the layout

--- src/pages/RootLayout/RootLayout.tsx
+++ src/pages/RootLayout/RootLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import classes from './RootLayout.module.css';
+import {  Container } from '@mui/material';
 
 const RootLayout = () => {
   return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+      <Container maxWidth="sm">
+        <Header />
+        <Outlet />
+        <Footer />
+      </Container>
   );
 }
 
