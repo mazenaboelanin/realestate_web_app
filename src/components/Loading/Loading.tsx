11c4379b7b2abed1a6730db9e1ef7f6cc6c09393
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-06-06 04:23:51 +0300

    Style: design loading component and fix apartment image

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,13 +1,25 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Skeleton, Stack, Typography } from "@mui/material";
 
 export const Loading: React.FC = () => {
+
   return (
     <>
       <Box>
-        <Typography variant="h3" gutterBottom>
+        <Typography variant="h3" gutterBottom sx={{ textAlign: 'center'}} mt={2}>
           Loading...
         </Typography>
       </Box>
+      <Stack spacing={1}>
+      { Array(5).fill(0).map((_, i) => (
+        <>
+          <Skeleton variant="text" sx={{ fontSize: '2rem' }} />
+          <Skeleton variant="rectangular" width={400} height={80} />
+          <Skeleton variant="rectangular" width={400} height={80} />
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+          <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+        </>
+      ))}
+    </Stack>
     </>
   );
 }
