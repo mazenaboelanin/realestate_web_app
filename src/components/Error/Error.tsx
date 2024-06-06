b57abb6324f7e56fdd415276806889b3eec1a2a7
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-06-06 11:03:08 +0300

    Style: design error component

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,4 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Grid, Typography } from "@mui/material";
+import classes from './Error.module.css';
+import ErrorIcon from '@mui/icons-material/Error';
+
 
 interface ErrorProps {
   children: React.ReactNode;
@@ -7,11 +10,24 @@ interface ErrorProps {
 export const Error: React.FC<ErrorProps> = ({children}) => {
   return (
     <>
-      <Box>
-        <Typography variant="h3" gutterBottom>
-          {children}
+    <Box className={ classes.container} maxWidth="sm" style={{ minHeight: '70vh'}} my={2}>
+      <Grid container spacing={2} style={{ width: '100%', height: '100%'}}>
+        <Grid item xs={12} md={12}  xl={12} className={ classes.imgGrid}>
+          <ErrorIcon fontSize="large" color="error" style={{ fontSize: '5rem'}}/>
+        </Grid>
+        <Grid item xs={12} md={12} xl={12}>
+        <Box sx={{ textAlign: 'center'}}>
+          <h2> Something went wrong!</h2>
+
+          <Typography gutterBottom>
+            <span> <span style={{ color: 'darkred'}}>Error Message:</span> {children}</span>
         </Typography>
-      </Box>
+        <h4>Please Check Again </h4>
+          <Button variant="contained" color="primary" onClick={() => window.location.reload()}>Reload</Button>
+        </Box>
+        </Grid>
+      </Grid>
+    </Box>
     </>
   );
 }
