3a99d31108bbf5a2c9c84af4c7a03149add50650
Mazen Abo Elanin Ahmed <mazenaboelanin@gmail.com>
2024-06-09 03:39:16 +0300

    Feat: add filters and order by feature

--- src/pages/apartments/ApartmentsListing/ApartmentsListing.tsx
+++ src/pages/apartments/ApartmentsListing/ApartmentsListing.tsx
@@ -7,9 +7,16 @@ import { ApartmentListCard } from "../../../components/ApartmentListCard/Apartme
 import { Loading } from "../../../components/Loading/Loading";
 import { EmptyState } from "../../../components/EmptyState/EmptyState";
 import { Error } from "../../../components/Error/Error";
-import { Box, Container, Pagination, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Pagination, Stack, Typography } from "@mui/material";
 import { useFetch } from "../../../hooks/useFetch";
 
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+const BASE_URL = 'http://localhost:5000/api/v1/apartments';
+const DEFAULT_URL = `${BASE_URL}?page=1&countPerPage=5`;
 
 export const ApartmentsListing: React.FC = () => {
   const navigate = useNavigate();
@@ -17,6 +24,10 @@ export const ApartmentsListing: React.FC = () => {
   const queryParams = new URLSearchParams(location.search);
   const pageFromUrl = Number(queryParams.get('page')) || 1;
   const [currentPage, setCurrentPage] = useState(pageFromUrl);
+  const [url, setUrl] = useState(`${BASE_URL}?page=${currentPage}&countPerPage=${5}`);
+  const [area, setArea] = useState('');
+  const [price, setPrice] = useState('');
+  const [orderBy, setOrderBy] = useState({orderBy: '', orderType: ''});
 
   const handleCurrentPageChange = (event: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
@@ -28,13 +39,157 @@ export const ApartmentsListing: React.FC = () => {
     metaData,
     isLoading,
     error
-  } = useFetch<Apartment[]>(`http://localhost:5000/api/v1/apartments?page=${currentPage}&countPerPage=${5}`, [], [currentPage]);
+  } = useFetch<Apartment[]>(url, [], [currentPage]);
+
+  const handleFilterChange = (event: SelectChangeEvent, filterType: string) => {
+    switch (filterType) {
+      case 'area':
+        setArea(event.target.value as string);
+        break;
+      case 'price':
+        setPrice(event.target.value as string);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleOrderChange = (event: SelectChangeEvent<unknown>, orderName: string) => {
+    switch (orderName) {
+      case 'orderBy':
+        setOrderBy({ ...orderBy, orderBy: event.target.value as string});
+        break;
+      case 'orderType':
+        setOrderBy({ ...orderBy, orderType: event.target.value as string});
+        break;
+      default:
+        break;
+    }
+  }
+
+  const prepareFilterObject = (filterRange: string) => {
+    const filterObj: {
+      min: number | null;
+      max: number | null;
+    } = { min: null, max: null};
+    
+    if (filterRange) {
+      const [min, max] = filterRange.split('-');
+      filterObj.min = Number(min);
+      filterObj.max = Number(max);
+    }
+    return filterObj;
+  }
+
+  const getAllFilters = () => {
+    const areaFilterObject = prepareFilterObject(area);
+    const priceFilterObject = prepareFilterObject(price);
+    let filters = {};
+    if(areaFilterObject.min && areaFilterObject.max) {
+      filters = { area: areaFilterObject };
+    }
+
+    if(priceFilterObject.min && priceFilterObject.max) {
+      filters = { ...filters, price: priceFilterObject };
+    }
+
+    return filters;
+  }
+
+  const handleApplyFiltersAndSort = () => {
+    let newUrl = DEFAULT_URL;
+    let filters = getAllFilters();
+
+    if (filters) {
+      newUrl += `&filterBy=${JSON.stringify(filters)}`;
+    }
+
+    if (orderBy.orderBy && orderBy.orderType) {
+      newUrl +=`&orderBy=${JSON.stringify(orderBy)}`;
+    }
+  
+    if (url) {
+      setUrl(newUrl);
+    }
+  }
 
   return (
     <>
-
 <Container maxWidth="sm" style={{ minHeight: '70vh'}}>
+  {/* FILTERING*/}
+  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  padding: '16px 16px 10px' }}>
+    <Typography variant="h6" mr={2}>Filter</Typography>
+    <FormControl sx={{ marginRight: '10px'}} fullWidth>
+      <InputLabel id="demo-simple-select-label">Area</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={area}
+        label="Area"
+        onChange={(event) => handleFilterChange(event, 'area')}
+      >
+        <MenuItem value={''}>Clear Area Filter</MenuItem>
+        <MenuItem value={'120-140'}>{ '120 - 140'}</MenuItem>
+        <MenuItem value={'150-170'}>{ '150 - 170'}</MenuItem>
+        <MenuItem value={'180-200'}>{ '180 - 200'}</MenuItem>
+      </Select>
+    </FormControl>
 
+    <FormControl sx={{ marginRight: '10px'}} fullWidth>
+      <InputLabel id="demo-simple-select-label">Price</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={price}
+        label="Price"
+        onChange={(event) => handleFilterChange(event, 'price')}
+      >
+        <MenuItem value={''}>Clear Price Filter</MenuItem>
+        <MenuItem value={'1-1.5'}>{ '1m - 1.5m'}</MenuItem>
+        <MenuItem value={'1.6-2'}>{ '1.6m - 2m'}</MenuItem>
+        <MenuItem value={'2-3'}>{ '2m - 3m'}</MenuItem>
+      </Select>
+    </FormControl>
+  </Box>
+
+    {/* SORTING */}
+  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '0px 16px'}}>
+  <Typography variant="h6" mr={2}>Order</Typography>
+    <FormControl sx={{ marginRight: '10px'}} fullWidth>
+      <InputLabel id="demo-simple-select-label">By</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        label="orderBy"
+        onChange={(event) => handleOrderChange(event, 'orderBy')}
+      >
+        <MenuItem value={''}>Clear order by</MenuItem>
+        <MenuItem value={'createdAt'}>{'Date'}</MenuItem>
+        <MenuItem value={'price'}>{ 'Price'}</MenuItem>
+        <MenuItem value={'area'}>{ 'Area'}</MenuItem>
+      </Select>
+    </FormControl>
+
+    <FormControl sx={{ marginRight: '10px'}} fullWidth>
+      <InputLabel id="demo-simple-select-label">Type</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        label="orderType"
+        onChange={(event) => handleOrderChange(event, 'orderType')}
+      >
+        <MenuItem value={''}>Clear order type</MenuItem>
+        <MenuItem value={'ASC'}>{ 'Ascending'}</MenuItem>
+        <MenuItem value={'DESC'}>{ 'Descending'}</MenuItem>
+      </Select>
+    </FormControl>
+
+  </Box>
+  <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',  padding: '10px 16px'}}>
+    <Button variant="contained" sx={{ width: '100%'}} onClick={handleApplyFiltersAndSort}>Apply</Button>
+  </Box>
+  
+      {/* PAGINATION */}
     {
       !isLoading && !error && apartments.length > 0 &&
       <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '16px'}}>
